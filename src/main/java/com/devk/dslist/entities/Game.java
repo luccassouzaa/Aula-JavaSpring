e59ea7f295b559a4f024eefe97239edeac90c47f
Luccas <luccasmelim@yahoo.com.br>
2023-05-10 13:36:59 -0300

    Atualization Errors

--- src/main/java/com/devk/dslist/entities/Game.java
+++ src/main/java/com/devk/dslist/entities/Game.java
@@ -21,22 +21,28 @@ public class Game {
 	@Column(name = "game_year") // mudar o nome do int year no sql
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT") //Definir um tamanho MAIOR de uma string
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT") //Definir um tamanho MAIOR de uma string
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(long id, String title, Integer year, String ganre, String plataform, String imgUrl,
+	public Game(long id, String title, Integer year, String ganre, String plataforms, Double score,String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = ganre;
-		this.platform = plataform;
+		this.platforms = plataforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -74,12 +80,12 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatform(String platforms) {
+		this.platforms = platforms;
 	}
 
 	public String getImgUrl() {
@@ -121,6 +127,14 @@ public class Game {
 			return false;
 		Game other = (Game) obj;
 		return id == other.id;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 	
 	
