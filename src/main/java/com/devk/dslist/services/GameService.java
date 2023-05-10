f02d7e9638f9d8900be592dfc080003921897029
Luccas <luccasmelim@yahoo.com.br>
2023-05-10 20:32:46 -0300

    Dynamic find

--- src/main/java/com/devk/dslist/services/GameService.java
+++ src/main/java/com/devk/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devk.dslist.dto.GameDTO;
 import com.devk.dslist.dto.GameMinDTO;
 import com.devk.dslist.entities.Game;
 import com.devk.dslist.repositories.GameRepository;
@@ -15,6 +17,15 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true) // assegurando que nao irei fazer nenhuma operação de escrita.
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		//CASO O ID NAO EXISTA, SERÁ FEITO UM TRATAMENTO DE EXCESSÕES 
+	
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		
 		List<Game> result = gameRepository.findAll();
