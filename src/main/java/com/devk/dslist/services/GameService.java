47b77c83b12b725f385f79a4088de68a8401bc58
Luccas <luccasmelim@yahoo.com.br>
2023-05-10 21:07:44 -0300

    SQL

--- src/main/java/com/devk/dslist/services/GameService.java
+++ src/main/java/com/devk/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devk.dslist.dto.GameDTO;
 import com.devk.dslist.dto.GameMinDTO;
 import com.devk.dslist.entities.Game;
+import com.devk.dslist.projections.GameMinProjection;
 import com.devk.dslist.repositories.GameRepository;
 
 @Service
@@ -29,6 +30,14 @@ public class GameService {
 	public List<GameMinDTO> findAll(){
 		
 		List<Game> result = gameRepository.findAll();
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
 	}
